656956240a9bef2924e52c2beb76cc915b3a0479
Stéphane Reiss <stephane.reiss@gmail.com>
2014-01-28 02:04:16 -0500

    Added standard deck test.
    Added fixtures files.

--- api_tests.py
+++ api_tests.py
@@ -7,10 +7,14 @@ class FlaskrTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls.test_username       = "deckbox_api"
+        cls.empty_deck_id       = "590775"
+        cls.standard_deck_id    = "590776"
+
         cls.app = api.app.test_client()
-        cls.test_username = "deckbox_api"
         rv = cls.app.get('/api/' + cls.test_username + "/")
         cls.profile_data = json.loads(rv.data)
+        cls.fixture_path = "tests/fixtures/"
 
     def setUp(self):
         pass
@@ -32,16 +36,16 @@ class FlaskrTestCase(unittest.TestCase):
           "will_trade": "My Continent"
         }
 
-        self.assertTrue("username" in user_profile_actual, "username doesn't exist.")
-        self.assertEqual(user_profile_expected["username"], user_profile_actual["username"], "username doesn't match.")
-        self.assertTrue("bio" in user_profile_actual, "bio doesn't exist.")
-        self.assertEqual(user_profile_expected["bio"], user_profile_actual["bio"], "bio doesn't match.")
-        self.assertTrue("feedback" in user_profile_actual, "username doesn't exist.")
-        self.assertEqual(user_profile_expected["feedback"], user_profile_actual["feedback"], "feedback doesn't match.")
-        self.assertTrue("location" in user_profile_actual, "username doesn't exist.")
-        self.assertEqual(user_profile_expected["location"], user_profile_actual["location"], "location doesn't match.")
-        self.assertTrue("will_trade" in user_profile_actual, "username doesn't exist.")
-        self.assertEqual(user_profile_expected["will_trade"], user_profile_actual["will_trade"], "will_trade field doesn't match.")
+        self.assertTrue("username" in user_profile_actual)
+        self.assertEqual(user_profile_expected["username"], user_profile_actual["username"])
+        self.assertTrue("bio" in user_profile_actual)
+        self.assertEqual(user_profile_expected["bio"], user_profile_actual["bio"])
+        self.assertTrue("feedback" in user_profile_actual)
+        self.assertEqual(user_profile_expected["feedback"], user_profile_actual["feedback"])
+        self.assertTrue("location" in user_profile_actual)
+        self.assertEqual(user_profile_expected["location"], user_profile_actual["location"])
+        self.assertTrue("will_trade" in user_profile_actual)
+        self.assertEqual(user_profile_expected["will_trade"], user_profile_actual["will_trade"])
 
     def test_api_profile_default_sets(self):
         user_profile_actual    = self.profile_data
@@ -53,31 +57,39 @@ class FlaskrTestCase(unittest.TestCase):
           ],
         }
 
-        self.assertEqual(len(user_profile_expected["sets"]), 3, "Sets count doesn't match.")
-        self.assertEqual(user_profile_expected["sets"][0]["id"], user_profile_actual["sets"][0]["id"], "Inventory id doesn't match.")
-        self.assertEqual(user_profile_expected["sets"][0]["name"], user_profile_actual["sets"][0]["name"], "Inventory name doesn't match.")
-        self.assertEqual(user_profile_expected["sets"][1]["id"], user_profile_actual["sets"][1]["id"], "Tradelist id doesn't match.")
-        self.assertEqual(user_profile_expected["sets"][1]["name"], user_profile_actual["sets"][1]["name"], "Tradelist name doesn't match.")
-        self.assertEqual(user_profile_expected["sets"][2]["id"], user_profile_actual["sets"][2]["id"], "Wishlist id doesn't match.")
-        self.assertEqual(user_profile_expected["sets"][2]["name"], user_profile_actual["sets"][2]["name"], "Wishlist name doesn't match.")
+        self.assertEqual(len(user_profile_expected["sets"]), 3)
+        self.assertEqual(user_profile_expected["sets"][0]["id"], user_profile_actual["sets"][0]["id"])
+        self.assertEqual(user_profile_expected["sets"][0]["name"], user_profile_actual["sets"][0]["name"])
+        self.assertEqual(user_profile_expected["sets"][1]["id"], user_profile_actual["sets"][1]["id"])
+        self.assertEqual(user_profile_expected["sets"][1]["name"], user_profile_actual["sets"][1]["name"])
+        self.assertEqual(user_profile_expected["sets"][2]["id"], user_profile_actual["sets"][2]["id"])
+        self.assertEqual(user_profile_expected["sets"][2]["name"], user_profile_actual["sets"][2]["name"])
 
     def test_api_empty_deck(self):
-        rv = self.app.get('/api/' + self.test_username + "/set/590775/")
+        rv = self.app.get('/api/' + self.test_username + "/set/" + self.empty_deck_id + "/")
         empty_deck_actual = json.loads(rv.data)
 
-        empty_deck_expected  = {
-          "cards": [],
-          "cards_count": {
-            "cards": 0,
-            "distinct": 0
-          },
-          "title": "Empty deck"
-        }
+        json_data = open(self.fixture_path + 'empty_deck.json')
+        empty_deck_expected = json.load(json_data)
+        json_data.close()
 
-        self.assertEqual(len(empty_deck_expected["cards"]), 0, "Empty deck should have no cards.")
-        self.assertEqual(empty_deck_expected["cards_count"]["cards"], empty_deck_actual["cards_count"]["cards"], "Cards count doesn't match.")
-        self.assertEqual(empty_deck_expected["cards_count"]["distinct"], empty_deck_actual["cards_count"]["distinct"], "Distinct cards count doesn't match.")
-        self.assertEqual(empty_deck_expected["title"], empty_deck_actual["title"], "title count doesn't match.")
+        self.assertEqual(len(empty_deck_expected["cards"]), 0)
+        self.assertEqual(empty_deck_expected["cards_count"]["cards"], empty_deck_actual["cards_count"]["cards"])
+        self.assertEqual(empty_deck_expected["cards_count"]["distinct"], empty_deck_actual["cards_count"]["distinct"])
+        self.assertEqual(empty_deck_expected["title"], empty_deck_actual["title"])
+
+    def test_api_standard_deck(self):
+        rv = self.app.get('/api/' + self.test_username + "/set/" + self.standard_deck_id + "/")
+        standard_deck_actual = json.loads(rv.data)
+
+        json_data = open(self.fixture_path + 'standard_deck.json')
+        standard_deck_expected = json.load(json_data)
+        json_data.close()
+
+        self.assertEqual(len(standard_deck_expected["cards"]), len(standard_deck_actual["cards"]))
+        self.assertEqual(standard_deck_expected["cards_count"]["cards"], standard_deck_actual["cards_count"]["cards"])
+        self.assertEqual(standard_deck_expected["cards_count"]["distinct"], standard_deck_actual["cards_count"]["distinct"])
+        self.assertEqual(standard_deck_expected["title"], standard_deck_actual["title"])
 
 if __name__ == '__main__':
     unittest.main()
